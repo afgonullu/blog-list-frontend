df11da55f6adab8a7767b363687551db719d7e01
A Faruk Gonullu <af@gonul.lu>
2020-11-21 17:08:04 +0300

    feat: add login, list and create blog functionality
    FSO: end of exercise 5.4
    All exercises are complete up to 5.4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,107 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react"
+import Blog from "./components/Blog"
+import InfoMessage from "./components/InfoMessage"
+import CreateBlog from "./components/CreateBlog"
+import blogService from "./services/blogs"
+import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
+  const [alert, setAlert] = useState({})
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogListUser")
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+      console.log(user)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      blogService.setToken(user.token)
+      setUser(user)
+      window.localStorage.setItem("loggedBlogListUser", JSON.stringify(user))
+      setUsername("")
+      setPassword("")
+      console.log(user)
+      setAlert({
+        message: `login successful for ${user.username}`,
+        type: "success",
+      })
+      setTimeout(() => {
+        setAlert({})
+      }, 3000)
+    } catch (exception) {
+      setAlert({ message: "Wrong credentials", type: "danger" })
+      setTimeout(() => {
+        setAlert({})
+      }, 3000)
+    }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem("loggedBlogListUser")
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h1>Blog List App by Afg</h1>
+        <InfoMessage message={alert.message} alertType={alert.type} />
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
+      <h1>Blog List App by Afg</h1>
+      <InfoMessage message={alert.message} alertType={alert.type} />
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <p>{user.name} is logged in.</p>
+      <button onClick={handleLogout}>Log Out</button>
+      <CreateBlog setAlert={setAlert} setBlogs={setBlogs}></CreateBlog>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
---
+++ src/components/CreateBlog.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react"
+import blogService from "../services/blogs"
+
+const CreateBlog = (props) => {
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
+
+  const handleCreate = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = {
+        title,
+        author,
+        url,
+      }
+      await blogService.create(newBlog)
+      props.setAlert({
+        message: `New Blog is Created: ${title} by ${author}`,
+        type: "success",
+      })
+      setTimeout(() => {
+        props.setAlert({})
+      }, 3000)
+      const blogs = await blogService.getAll()
+      props.setBlogs(blogs)
+      setTitle("")
+      setAuthor("")
+      setUrl("")
+    } catch (error) {
+      props.setAlert({
+        message: `All Fields must be properly filled.`,
+        type: "danger",
+      })
+      setTimeout(() => {
+        props.setAlert({})
+      }, 3000)
+    }
+  }
+
+  return (
+    <form onSubmit={handleCreate}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  )
+}
+
+export default CreateBlog
